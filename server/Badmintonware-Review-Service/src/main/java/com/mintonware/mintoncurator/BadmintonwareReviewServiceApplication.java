86dd7c5b243fbc6a6aaa37bcfb3c994f5d5f7b78
waldsteiner <waldsteiner@gmail.com>
2022-11-11 01:39:36 +0900

    first setting

---
+++ server/Badmintonware-Review-Service/src/main/java/com/mintonware/mintoncurator/BadmintonwareReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mintonware.mintoncurator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadmintonwareReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadmintonwareReviewServiceApplication.class, args);
+	}
+
+}
---
+++ server/Badmintonware-Review-Service/src/test/java/com/mintonware/mintoncurator/BadmintonwareReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mintonware.mintoncurator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadmintonwareReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
