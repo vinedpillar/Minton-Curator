d5e09aaef3332c5c227b9fbb931b0afe3c319d78
waldsteiner <waldsteiner@gmail.com>
2022-11-13 23:59:15 +0900

    Update Basic Setting

---
+++ server/Badmintonware-Review-Service/src/main/java/com/mintonware/mintoncurator/domain/member/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.mintonware.mintoncurator.domain.member.service;
+
+public interface MemberService {
+    // 회원 생성
+    // 회원 정보 확인
+    // 모든 회원 정보 확인
+    // 회원 정보 수정
+    // 회원 정보 삭제
+    // 회원 정보가 있는지 확인
+    // 회원 정보 검
+}
